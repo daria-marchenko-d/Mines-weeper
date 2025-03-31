e132aa75b77301f986536ceb3ddfd7575cc4f0fc
daria-marchenko-d <daria.marchenko@laplateforme.io>
2025-03-31 09:40:41 +0200

    Change score page

--- main.py
+++ main.py
@@ -7,15 +7,3 @@ if __name__ == "__main__":
     
     game = Minesweeper()
     game.run()
-# import pygame
-# from game import Minesweeper
-
-# if __name__ == "__main__":
-#     pygame.init()
-#     pygame.font.init()
-    
-#     try:
-#         game = Minesweeper()
-#         game.run()
-#     finally:
-#         pygame.quit()
